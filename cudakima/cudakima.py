da60304a4a39b0952fe8e0260dea461d3b845749
Alessandro Santini <asantini@saraswati.aei.mpg.de>
2025-08-28 16:01:24 +0200

    adjusted version

--- cudakima/cudakima.py
+++ cudakima/cudakima.py
@@ -2,7 +2,7 @@
 
 if __name__ != "__main__":
     __name__                = "cudakima"
-    __version__             = "0.0.3"
+    __version__             = "0.1.0"
     __author__              = "Alessandro Santini"
     __author_email__        = "alessandro.santini@aei.mpg.de"
     __description__         = "GPU-accelerated, parallel implementation of Akima Splines"
