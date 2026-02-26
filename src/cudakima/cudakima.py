719002337379a24a9359d5fc33f7bdb36e12cb92
Alessandro Santini <alesandro.santini@aei.mpg.de>
2026-02-26 09:17:39 +0100

    dynamic versioning + version bump

--- src/cudakima/cudakima.py
+++ src/cudakima/cudakima.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+from importlib.metadata import version as _version
 
 if __name__ != "__main__":
     __name__                = "cudakima"
-    __version__             = "0.2.2"
+    __version__             = _version("cudakima")
     __author__              = "Alessandro Santini"
     __author_email__        = "alessandro.santini@aei.mpg.de"
     __description__         = "GPU-accelerated, parallel implementation of Akima Splines"
